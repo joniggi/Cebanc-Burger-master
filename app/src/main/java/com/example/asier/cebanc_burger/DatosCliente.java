a5c821a7125635e9ad517911ee8e570cbacd1491
joniggi <patexample04@gmail.com>
2017-02-03 13:22:54 +0100

    holita loko

--- app/src/main/java/com/example/asier/cebanc_burger/DatosCliente.java
+++ app/src/main/java/com/example/asier/cebanc_burger/DatosCliente.java
@@ -3,18 +3,24 @@ package com.example.asier.cebanc_burger;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class DatosCliente extends MainActivity{
 
     //definimos las varible que vamos a utilizar
     private ImageView siguiente;
     private ImageView salir;
+    private Button comprobar;
     private EditText nombre, apellidos, direccion, telefono;
     private boolean verificar = false;
 
@@ -26,18 +32,71 @@ public class DatosCliente extends MainActivity{
        //identificamos los objetos del xml
         siguiente = (ImageView) findViewById(R.id.buttonSiguiente);
         salir = (ImageView) findViewById(R.id.buttonSalir);
-        nombre = (EditText) findViewById(R.id.editTextNombre);
         apellidos = (EditText) findViewById(R.id.editTextApellido);
+        comprobar =(Button)findViewById(R.id.comprobar);
+
+        nombre = (EditText) findViewById(R.id.editTextNombre);
         direccion = (EditText) findViewById(R.id.editTextDireccion);
         telefono = (EditText) findViewById(R.id.editTextTelefono);
 
+
+        /*direccion.setEnabled(false);
+        telefono.setEnabled(false);*/
 
 
         siguiente.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                verificar = comprobacion();
-                lanzarPedido(verificar);
+                String n = nombre.getText().toString();
+                String a = apellidos.getText().toString();
+                String d = direccion.getText().toString();
+                String t = telefono.getText().toString();
+                String mensajeValidacion = "";
+
+                if(nombreExiste(n)){
+                    Toast toastBienvenido = Toast.makeText(getApplicationContext(),
+                            "Hola, " + n + " bienvenido", Toast.LENGTH_SHORT);
+                    toastBienvenido.show();
+                    lanzarActividad();
+
+                }else{
+                    Boolean nombreCorrecto = validarNombre(n);
+                    Boolean apellidoCorrecto = validarApellidos(a);
+                    Boolean direccionCorrecto = validarDireccion(d);
+                    Boolean telefonoCorrecto = validarTelefono(t);
+
+                    if(!nombreCorrecto){
+                        nombre.setError("Debes introducir el nombre");
+                    }
+                    if(!apellidoCorrecto){
+                        apellidos.setError("Debes introducir el apellido");
+                    }
+                    if(!direccionCorrecto){
+                        direccion.setError("Debes introducir el direccion");
+                    }
+                    if(!telefonoCorrecto){
+                        telefono.setError("Debes introducir el telefono");
+                    }
+
+                    if(!nombreCorrecto || !direccionCorrecto || !telefonoCorrecto){
+                        Toast toastError = Toast.makeText(getApplicationContext(),
+                                "Debes introducir los datos", Toast.LENGTH_SHORT);
+                        toastError.show();
+                    }else{
+                        insertarNombre(n, a, d, t);
+                        Toast toastBienvenido = Toast.makeText(getApplicationContext(),
+                                "Hola, " + n + " bienvenido", Toast.LENGTH_SHORT);
+                        toastBienvenido.show();
+                        lanzarActividad();
+
+                    }
+                }
+            }
+        });
+        comprobar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+             comprobar2();
             }
         });
 
@@ -55,16 +114,173 @@ public class DatosCliente extends MainActivity{
 
     }
 
+
+
+    public void insertarNombre(String nombre, String apellidos, String direccion, String telefono){
+        HamburSQLiteHelper burguer_dbh =
+                new HamburSQLiteHelper(this, "DBBurguer", null, 2);
+        SQLiteDatabase db = burguer_dbh.getWritableDatabase();
+
+        db.execSQL("INSERT INTO USUARIOS (USUARIO, NOMBRE_USU, APELLIDOS, DIRECCION, TELF) VALUES ( "
+                + null + ", '"+ nombre +"', '"+ apellidos +"',  '" + direccion + "', '" + telefono + "' )");
+
+        Toast toastCarrito = Toast.makeText(getApplicationContext(),
+                "¡El usuario se ha registrado en la base de datos!", Toast.LENGTH_SHORT);
+        toastCarrito.show();
+
+        db.close();
+    }
+
+    public boolean nombreExiste(String nombre){
+        HamburSQLiteHelper burguer_dbh =
+                new HamburSQLiteHelper(this, "DBBurguer", null, 2);
+        SQLiteDatabase db = burguer_dbh.getReadableDatabase();
+
+        Cursor c = db.rawQuery("SELECT NOMBRE_USU FROM USUARIOS WHERE NOMBRE_USU = '" + nombre + "'", null);
+
+        if(c.getCount() > 0){
+            c.close();
+            db.close();
+            return true;
+        }
+        c.close();
+        db.close();
+        return false;
+
+    }
+
+    public boolean validarNombre(String n) {
+        String nombre = n;
+
+        if(nombre.length()!=0){
+            return true;
+        }
+        return false;
+
+    }
+    public boolean validarApellidos(String a) {
+        String apellidos = a;
+
+        if(apellidos.length()!=0){
+            return true;
+        }
+        return false;
+
+    }
+
+    public boolean validarDireccion(String d) {
+        String direccion = d;
+
+        if(direccion.length()!=0){
+            return true;
+        }
+        return false;
+
+    }
+
+    public boolean validarTelefono(String t) {
+        String telefono = t;
+
+        if(telefono.length()==9 && telefono.matches("\\d*")){
+            return true;
+        }
+        return false;
+
+    }
+
+
+
+
+public void comprobar2(){
+    String n = nombre.getText().toString();
+
+    if(nombreExiste(n)){
+        apellidos.setEnabled(false);
+        direccion.setEnabled(false);
+        telefono.setEnabled(false);
+
+        HamburSQLiteHelper burguer_dbh =
+                new HamburSQLiteHelper(this, "DBBurguer", null, 2);
+        SQLiteDatabase db = burguer_dbh.getWritableDatabase();
+
+        Cursor c = db.rawQuery("SELECT APELLIDOS, DIRECCION, TELF FROM USUARIOS WHERE NOMBRE_USU = '" + n + "'", null);
+
+        String apellidosDB = "";
+        String direccionDB = "";
+        String telefonoDB = "";
+        try{
+            if(c.moveToFirst()){
+                do{
+                    apellidosDB = c.getString(0);
+                    direccionDB = c.getString(1);
+                    telefonoDB = c.getString(2);
+                }while(c.moveToNext());
+            }
+        }catch(Exception ex){
+
+        }
+        c.close();
+        db.close();
+
+
+        Toast toastUsuarioExiste = Toast.makeText(getApplicationContext(),
+                "¡El usuario introducido ya existe!", Toast.LENGTH_SHORT);
+        toastUsuarioExiste.show();
+
+        apellidos.setText(apellidosDB);
+        direccion.setText(direccionDB);
+        telefono.setText(telefonoDB);
+
+
+
+    }else{
+        apellidos.setEnabled(true);
+        direccion.setEnabled(true);
+        telefono.setEnabled(true);
+
+        Toast toastUsuarioNoExiste = Toast.makeText(getApplicationContext(),
+                "¡El usuario introducido no existe, introduce tus datos!", Toast.LENGTH_SHORT);
+        toastUsuarioNoExiste.show();
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     //este metodo recibe un booleano y en caso de que sea true lanza la actividad
     public void lanzarPedido(boolean v) {
-        if (v == true) {
-            Intent i = new Intent(DatosCliente.this, AnadirHamburguesas.class);
-            i.putExtra("nombre", nombre.getText().toString());
-            i.putExtra("apellidos", apellidos.getText().toString());
-            i.putExtra("direccion", direccion.getText().toString());
-            i.putExtra("telefono", telefono.getText().toString());
-            startActivityForResult(i, 1234);
-            overridePendingTransition(R.anim.left_in, R.anim.left_out);
+        String n=nombre.getText().toString();
+        String d=direccion.getText().toString();
+        String t=telefono.getText().toString();
+        if (v == true && usuarioExiste(n) ) {
+            Toast toastBienvenido = Toast.makeText(getApplicationContext(),
+                    "¡Bienvenido, " + n + " !", Toast.LENGTH_SHORT);
+            toastBienvenido.show();
+            lanzarActividad();
+        }else if(v == true && usuarioExiste(n)==false){
+            insertarUsuario(n,d,t);
+            Toast toastInsertado = Toast.makeText(getApplicationContext(),
+                    "¡Usuario, " + n + " insertado", Toast.LENGTH_SHORT);
+            toastInsertado.show();
+            lanzarActividad();
         }
     }
 
@@ -118,6 +334,99 @@ public class DatosCliente extends MainActivity{
         });
         dialogo1.show();
 
+    }
+
+    public void lanzarActividad(){
+        Intent i = new Intent(DatosCliente.this, AnadirHamburguesas.class);
+        i.putExtra("nombre", nombre.getText().toString());
+        i.putExtra("apellidos", apellidos.getText().toString());
+        i.putExtra("direccion", direccion.getText().toString());
+        i.putExtra("telefono", telefono.getText().toString());
+        startActivityForResult(i, 1234);
+        overridePendingTransition(R.anim.left_in, R.anim.left_out);
+    }
+
+    public boolean usuarioExiste(String nombre){
+        HamburSQLiteHelper burguer_dbh =
+                new HamburSQLiteHelper(this, "DBBurguer", null, 2);
+        SQLiteDatabase db = burguer_dbh.getReadableDatabase();
+
+        Cursor c = db.rawQuery("SELECT NOMBRE_USU, DIRECCION, TELF FROM USUARIOS WHERE NOMBRE_USU = '" + nombre + "'", null);
+
+        if(c.getString(0).length() > 0 && c.getString(1).length() > 0 && c.getString(2).length() > 0 ){
+            c.close();
+            db.close();
+            return true;
+        }
+        c.close();
+        db.close();
+        return false;
+
+    }
+
+    public void insertarUsuario(String nombre, String direccion, String telefono){
+        HamburSQLiteHelper burguer_dbh =
+                new HamburSQLiteHelper(this, "DBBurguer", null, 2);
+        SQLiteDatabase db = burguer_dbh.getWritableDatabase();
+
+        db.execSQL("INSERT INTO USUARIOS (USUARIO, NOMBRE_USU, DIRECCION, TELF) VALUES ( "
+                + null + ", '"+ nombre +"', '" + direccion + "', '" + telefono + "' )");
+
+        /*Toast toastCarrito = Toast.makeText(getApplicationContext(),
+                "¡El usuario se ha registrado en la base de datos!", Toast.LENGTH_SHORT);
+        toastCarrito.show();*/
+
+        db.close();
+    }
+
+    public void comprobar(){
+        String n = nombre.getText().toString();
+
+        if(usuarioExiste(n)){
+            direccion.setEnabled(false);
+            telefono.setEnabled(false);
+
+            HamburSQLiteHelper burguer_dbh =
+                    new HamburSQLiteHelper(this, "DBBurguer", null, 2);
+            SQLiteDatabase db = burguer_dbh.getWritableDatabase();
+
+            Cursor c = db.rawQuery("SELECT DIRECCION, TELF FROM USUARIOS WHERE NOMBRE_USU = '" + n + "'", null);
+
+            String direccionDB = "";
+            String telefonoDB = "";
+            try{
+                if(c.moveToFirst()){
+                    do{
+                        direccionDB = c.getString(0);
+                        telefonoDB = c.getString(1);
+                    }while(c.moveToNext());
+                }
+            }catch(Exception ex){
+
+            }
+            c.close();
+            db.close();
+
+
+            Toast toastUsuarioExiste = Toast.makeText(getApplicationContext(),
+                    "¡El usuario introducido ya existe!", Toast.LENGTH_SHORT);
+            toastUsuarioExiste.show();
+
+            direccion.setText(direccionDB);
+            telefono.setText(telefonoDB);
+
+
+
+        }else{
+            direccion.setEnabled(true);
+            telefono.setEnabled(true);
+
+            Toast toastUsuarioNoExiste = Toast.makeText(getApplicationContext(),
+                    "¡El usuario introducido no existe, introduce tus datos!", Toast.LENGTH_SHORT);
+            toastUsuarioNoExiste.show();
+
+
+        }
     }
 
 }
--- app/src/main/java/com/example/asier/cebanc_burger/MainActivity.java
+++ app/src/main/java/com/example/asier/cebanc_burger/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.asier.cebanc_burger;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
@@ -23,6 +28,7 @@ public class MainActivity extends AppCompatActivity implements
     private ImageView entrar;
     private MediaPlayer mp3;
     private ImageView internet;
+    private FloatingActionButton basedatos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,12 +42,24 @@ public class MainActivity extends AppCompatActivity implements
         entrar = (ImageView) findViewById(R.id.entrar);
         internet = (ImageView) findViewById(R.id.internet);
         mp3 = MediaPlayer.create(this,R.raw.moneda);
+        basedatos = (FloatingActionButton) findViewById(R.id.basedatos);
 
         //hemos tenido que poner sin metodo porque sino nos cascaba la app
+
 
         TabLayout tabs = (TabLayout) findViewById(R.id.tabs5);
         tabs.addTab(tabs.newTab().setText("BIENVENIDO").setIcon(R.drawable.logom));
 
+        HamburSQLiteHelper burguer_dbh =
+                new HamburSQLiteHelper(this, "DBBurguer", null, 1);
+
+        SQLiteDatabase db = burguer_dbh.getWritableDatabase();
+
+        //Si hemos abierto correctamente la base de datos
+        if(db != null)
+        {//Cerramos la base de datos
+            db.close();
+        }
 
         entrar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -64,12 +82,59 @@ public class MainActivity extends AppCompatActivity implements
                 startActivity(intent);
             }
         });
+        basedatos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                comprobarBasedatos(v);
+            }
+        });
 
     }
 
+    /*public void runBaseDeDatos(){
+
+        HamburSQLiteHelper burguer_dbh =
+                new HamburSQLiteHelper(this, "DBBurguer", null, 1);
+
+        SQLiteDatabase db = burguer_dbh.getWritableDatabase();
+
+        //Si hemos abierto correctamente la base de datos
+        if(db != null)
+        {//Cerramos la base de datos
+            db.close();
+        }
+    }*/
+
+    public void comprobarBasedatos(View view){
+        HamburSQLiteHelper kebab_dbh =
+                new HamburSQLiteHelper(this, "DBBurguer", null, 2);
+        SQLiteDatabase db = kebab_dbh.getReadableDatabase();
+
+        Cursor ce = db.rawQuery("SELECT USUARIO, NOMBRE_USU, APELLIDOS, DIRECCION, TELF FROM USUARIOS", null);
+
+        try{
+            if(ce.moveToFirst()){
+                do{
+                    int usuario = ce.getInt(0);
+                    String nombre_usuario = ce.getString(1);
+                    String apellidos = ce.getString(2);
+                    String direccion = ce.getString(3);
+                    String telefono = ce.getString(4);
+
+                    Toast toastCarrito = Toast.makeText(getApplicationContext(),
+                            Integer.toString(usuario) + nombre_usuario + apellidos + direccion + telefono, Toast.LENGTH_SHORT);
+                    toastCarrito.show();
+                }while(ce.moveToNext());
+            }
+        }catch(Exception ex){
+
+        }
+
+        ce.close();
+        db.close();
 
 
-
+    }
 
 
     public void onMapReady(GoogleMap googleMap) {
